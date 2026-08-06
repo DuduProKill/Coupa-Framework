9d8a6774e41cf846b3d0d9aebffd9f3780e2818f
Eduardo Rafael <rafaelwork.br@gmail.com>
2026-08-06 10:48:56 -0300

    chore: bump version to v1.1.2

--- modules/updater.py
+++ modules/updater.py
@@ -10,7 +10,7 @@ from PyQt6.QtWidgets import QApplication, QMessageBox, QProgressDialog
 logger = logging.getLogger(__name__)
 
 GITHUB_REPO = "DuduProKill/Coupa-Framework"
-CURRENT_VERSION = "1.1.1"
+CURRENT_VERSION = "1.1.2"
 
 
 def _normalize_version(value: str) -> Optional[tuple[int, int, int]]:
